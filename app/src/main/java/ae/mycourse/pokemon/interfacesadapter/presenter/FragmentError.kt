09e219307b4268af7303f33cb133c61239e7047d
Alejandro Exposito <79151983+AlejandroDAM29@users.noreply.github.com>
2022-11-26 21:40:48 +0100

    Arreglos

--- app/src/main/java/ae/mycourse/pokemon/interfacesadapter/presenter/FragmentError.kt
+++ app/src/main/java/ae/mycourse/pokemon/interfacesadapter/presenter/FragmentError.kt
@@ -8,38 +8,17 @@ import android.view.ViewGroup
 import ae.mycourse.pokemon.R
 import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentError.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentError : Fragment() {
-
-    var datos: String? = null
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_error, container, false)
-
-
         val textError : TextView = view.findViewById(R.id.text_error)
         val args = this.arguments
         val inputData = args?.get("fragmentData")
         textError.text = inputData.toString()
-
         return view
     }
 }
