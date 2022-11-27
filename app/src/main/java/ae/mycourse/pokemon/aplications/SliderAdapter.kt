8608ae05e4bcf91048ebd1fb1df153585e7be42e
Alejandro Exposito <79151983+AlejandroDAM29@users.noreply.github.com>
2022-11-27 20:12:37 +0100

    Eliminada clase slider

--- app/src/main/java/ae/mycourse/pokemon/aplications/SliderAdapter.kt
+++
@@ -1,7 +0,0 @@
-package ae.mycourse.pokemon.aplications
-
-import com.smarteist.autoimageslider.SliderViewAdapter
-
-class SliderAdapter(): SliderViewAdapter<SliderAdapter.SliderAdapterViewHolder>() {
-    private val mSliderItems: List<Slider>? = null
-}
