646c5e36b0713551fa97cfa38f20ce8b0c486ab4
Alejandro Exposito <79151983+AlejandroDAM29@users.noreply.github.com>
2023-01-03 18:59:16 +0100

    Preparado para room

--- app/src/main/java/ae/mycourse/pokemon/framework/ui/pokemonlist/PokemonList.kt
+++ app/src/main/java/ae/mycourse/pokemon/framework/ui/pokemonlist/PokemonList.kt
@@ -1,5 +1,6 @@
 package ae.mycourse.pokemon.framework.ui.pokemonlist
 
+import ae.mycourse.pokemon.R
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,22 +8,25 @@ import android.view.View
 import android.view.ViewGroup
 import ae.mycourse.pokemon.databinding.FragmentPokemonListBinding
 import ae.mycourse.pokemon.domain.ListPokemonsModel
+import ae.mycourse.pokemon.framework.common.onTextChanged
 import android.annotation.SuppressLint
+import android.text.Editable
 import android.util.Log
 import androidx.appcompat.widget.SearchView
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class PokemonList : Fragment(), SearchView.OnQueryTextListener {
+class PokemonList : Fragment() {
 
     private var _binding: FragmentPokemonListBinding? = null
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
     private val pokemonListViewModel : PokemonListViewModel by viewModels()
-    lateinit var recyclerListAdapter: MyRecyclerAdapter
+    private lateinit var recyclerListAdapter: MyRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,18 +48,20 @@ class PokemonList : Fragment(), SearchView.OnQueryTextListener {
             recyclerListAdapter.notifyDataSetChanged()
         }
 
-        binding.searchbarList.setOnQueryTextListener(this)
-
+        binding.searchbarList.addTextChangedListener {
+           showIconSearchbar(it)
+            recyclerListAdapter.pokemonList = pokemonListViewModel.filterList(it.toString().lowercase())
+            recyclerListAdapter.notifyDataSetChanged()
+        }
         pokemonListViewModel.getPokemonList(requireContext())
         return view
     }
 
-    override fun onQueryTextSubmit(query: String?): Boolean = true
-
-    @SuppressLint("NotifyDataSetChanged")
-    override fun onQueryTextChange(newText: String?): Boolean {
-        recyclerListAdapter.pokemonList = pokemonListViewModel.filterList(newText)
-        recyclerListAdapter.notifyDataSetChanged()
-        return true
+    private fun showIconSearchbar(editable: Editable?){
+        var icon: Int = 0
+        if (editable.toString() == "")
+            icon = R.drawable.loupe
+        binding.searchbarList.setCompoundDrawablesWithIntrinsicBounds(icon,0,0,0)
     }
 }
+
