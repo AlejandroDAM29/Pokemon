2f1464d2f69918c536e8f883713a2743bb8cd8cc
Alejandro Exposito <79151983+AlejandroDAM29@users.noreply.github.com>
2023-01-08 18:51:11 +0100

    Video frame integrado

--- app/src/main/java/ae/mycourse/pokemon/framework/ui/introvideo/IntroVideo.kt
+++ app/src/main/java/ae/mycourse/pokemon/framework/ui/introvideo/IntroVideo.kt
@@ -7,11 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import ae.mycourse.pokemon.R
 import ae.mycourse.pokemon.databinding.FragmentIntroVideoBinding
+import ae.mycourse.pokemon.framework.ui.mainscreen.MainScreen
+import android.media.session.MediaController
+import android.media.session.MediaSession
+import android.net.Uri
+import android.provider.MediaStore.Images.Media
+import android.util.Log
+import androidx.fragment.app.commit
 
 class IntroVideo : Fragment() {
 
     private var _binding: FragmentIntroVideoBinding? = null
     private val binding get() = _binding!!
+    private var mediaControls: MediaController? = null
 
    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,8 +28,26 @@ class IntroVideo : Fragment() {
         _binding = FragmentIntroVideoBinding.inflate(inflater,container, false)
        val view = binding.root
 
-
+       binding.videoViewIntro.setVideoURI(Uri.parse("android.resource://" +
+               requireActivity().packageName +"/"+
+               R.raw.intro_pokemon))
+       binding.videoViewIntro.requestFocus()
+       binding.videoViewIntro.setOnPreparedListener {
+           binding.videoViewIntro.start()
+       }
+       binding.videoViewIntro.setOnClickListener {nextFragment()}
+       binding.videoViewIntro.setOnCompletionListener {nextFragment()}
 
        return view
     }
+
+    private fun nextFragment(){
+        parentFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(R.id.fragmentContainer, MainScreen())
+            addToBackStack("mainStackFragments")
+        }
+    }
+
+
 }
--- app/src/main/java/ae/mycourse/pokemon/framework/ui/main/MainActivity.kt
+++ app/src/main/java/ae/mycourse/pokemon/framework/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package ae.mycourse.pokemon.framework.ui.main
 import ae.mycourse.pokemon.R
 import ae.mycourse.pokemon.framework.common.longToast
 import ae.mycourse.pokemon.framework.common.shorToast
+import ae.mycourse.pokemon.framework.ui.introvideo.IntroVideo
 import ae.mycourse.pokemon.framework.ui.mainscreen.MainScreen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -26,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         if(savedInstanceState == null) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
-                add<MainScreen>(R.id.fragmentContainer)
+                add<IntroVideo>(R.id.fragmentContainer)
             }
 
         }
