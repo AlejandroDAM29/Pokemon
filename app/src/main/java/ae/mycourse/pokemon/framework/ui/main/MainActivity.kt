6ebe004d129f63c0df6929f8af2d8989f1a3acf5
Alejandro Exposito <79151983+AlejandroDAM29@users.noreply.github.com>
2023-01-08 15:55:38 +0100

    puesto el splas_screen

--- app/src/main/java/ae/mycourse/pokemon/framework/ui/main/MainActivity.kt
+++ app/src/main/java/ae/mycourse/pokemon/framework/ui/main/MainActivity.kt
@@ -6,6 +6,8 @@ import ae.mycourse.pokemon.framework.common.shorToast
 import ae.mycourse.pokemon.framework.ui.mainscreen.MainScreen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,8 +15,14 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val screenSplash: SplashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        screenSplash.setKeepOnScreenCondition{false}
+
         if(savedInstanceState == null) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
