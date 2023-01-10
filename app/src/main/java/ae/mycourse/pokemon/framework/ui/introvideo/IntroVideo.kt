585be82e5f45a0a4bdb79da9a847cf3d25d8df8d
Alejandro Exposito <79151983+AlejandroDAM29@users.noreply.github.com>
2023-01-10 20:33:54 +0100

    video renderizado

--- app/src/main/java/ae/mycourse/pokemon/framework/ui/introvideo/IntroVideo.kt
+++ app/src/main/java/ae/mycourse/pokemon/framework/ui/introvideo/IntroVideo.kt
@@ -1,25 +1,20 @@
 package ae.mycourse.pokemon.framework.ui.introvideo
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import ae.mycourse.pokemon.R
 import ae.mycourse.pokemon.databinding.FragmentIntroVideoBinding
 import ae.mycourse.pokemon.framework.ui.mainscreen.MainScreen
-import android.media.session.MediaController
-import android.media.session.MediaSession
 import android.net.Uri
-import android.provider.MediaStore.Images.Media
-import android.util.Log
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 
 class IntroVideo : Fragment() {
 
     private var _binding: FragmentIntroVideoBinding? = null
     private val binding get() = _binding!!
-    private var mediaControls: MediaController? = null
 
    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,17 +22,11 @@ class IntroVideo : Fragment() {
     ): View? {
         _binding = FragmentIntroVideoBinding.inflate(inflater,container, false)
        val view = binding.root
-
-       binding.videoViewIntro.setVideoURI(Uri.parse("android.resource://" +
-               requireActivity().packageName +"/"+
-               R.raw.intro_pokemon))
+       binding.videoViewIntro.setVideoURI(Uri.parse("android.resource://" + requireActivity().packageName +"/"+ R.raw.intro_pokemon))
        binding.videoViewIntro.requestFocus()
-       binding.videoViewIntro.setOnPreparedListener {
-           binding.videoViewIntro.start()
-       }
-       binding.videoViewIntro.setOnClickListener {nextFragment()}
+       binding.videoViewIntro.setOnPreparedListener{it.start()}
+       view.setOnClickListener {nextFragment()}
        binding.videoViewIntro.setOnCompletionListener {nextFragment()}
-
        return view
     }
 
@@ -45,7 +34,7 @@ class IntroVideo : Fragment() {
         parentFragmentManager.commit {
             setReorderingAllowed(true)
             replace(R.id.fragmentContainer, MainScreen())
-            addToBackStack("mainStackFragments")
+
         }
     }
 
