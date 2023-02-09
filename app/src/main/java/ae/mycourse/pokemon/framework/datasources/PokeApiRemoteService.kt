89e536210cb9cb9f96d90f0ceae84cebf04f0271
Alejandro Exposito <79151983+AlejandroDAM29@users.noreply.github.com>
2023-02-09 15:33:30 +0100

    Borrado codigo inservible

--- app/src/main/java/ae/mycourse/pokemon/framework/datasources/PokeApiRemoteService.kt
+++ app/src/main/java/ae/mycourse/pokemon/framework/datasources/PokeApiRemoteService.kt
@@ -5,14 +5,6 @@ import retrofit2.http.GET
 
 interface PokeApiRemoteService {
 
-    /*@GET(".")
-    suspend fun getURLBaseContent(): URLBaseResponse
-
-    @GET("pokemon?limit=150&offset=0")
-    suspend fun getListPokemon(): AllPokemonsModelResponse
-
-    @GET(value = "pokemon/{id}")
-    suspend fun getDetailsPokemon(@Path("id") id: String): DetailsPokemonModelResponse*/
     @GET("https://www.alejandroexpdeveloper.com/backend_pokemon/endpoints/pokemon_list.php")
     suspend fun getListPokemon(): MutableList<ListPokemonResponse>
 }
--- app/src/main/java/ae/mycourse/pokemon/framework/datasources/ServerPokemonDataSource.kt
+++ app/src/main/java/ae/mycourse/pokemon/framework/datasources/ServerPokemonDataSource.kt
@@ -6,19 +6,6 @@ import android.util.Log
 
 class ServerPokemonDataSource(private val pokeApiRemoteService: PokeApiRemoteService): PokemonApiService {
 
-    /*override suspend fun getListNamesPokemon(): MutableList<String> {
-        val allPokemon = pokeApiRemoteService.getListPokemon()
-        var pokemonNames: MutableList<String> = mutableListOf()
-        for(result in allPokemon.results){
-            pokemonNames.add(result.name)
-        }
-        return pokemonNames
-    }
-
-    override suspend fun getImagePokemon(id: String): String {
-        return pokeApiRemoteService.getDetailsPokemon(id).sprites.front_default
-    }*/
-
     override suspend fun getPokemonList(): ListPokemonsModel {
         val response = pokeApiRemoteService.getListPokemon()
         Log.i("Test 1", response.toString())
